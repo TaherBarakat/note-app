167f9a86564ee5a68ec32481f0b2fcc521873921
Taher Barakat <taher.barakat12223@gmail.com>
2024-02-19 15:57:00 +0300

    apply routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
 export default function App() {
-     return <div>App</div>;
+     return (
+          <Container className="my-4">
+               <Routes>
+                    <Route path="/" element={<h1>home</h1>}></Route>
+                    <Route path="/new" element={<h1>new</h1>}></Route>
+                    <Route path="/:id">
+                         <Route index element={<h1>show</h1>}></Route>
+                         <Route path="edit" element={<h1>edit</h1>}></Route>
+                    </Route>
+                    <Route path="*" element={<Navigate to="/" />}></Route>
+               </Routes>
+          </Container>
+     );
 }
