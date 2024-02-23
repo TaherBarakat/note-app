6c7341bf6f0ad21f8634066d5a07aa34868d69f0
Taher Barakat <taher.barakat12223@gmail.com>
2024-02-23 13:43:31 +0300

    edit type

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -31,6 +31,8 @@ type EditTagsModal = {
      show: boolean;
      availableTags: Tag[];
      handleClose: () => void;
+     onDeleteTag: (id: string) => void;
+     onUpdateTag: (id: string, label: string) => void;
 };
 export default function NoteList({
      availableTags,
